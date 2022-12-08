42a84fabdf2a79f3871f4d05c0510f2408b05cd5
Daikael <45525343+Daikael@users.noreply.github.com>
2022-12-08 14:29:02 -0700

    indentation
    tabs.

--- settings.lua
+++ settings.lua
@@ -6,7 +6,15 @@ data:extend(
 			setting_type = "startup",
 			default_value = false,
 			per_user = false,
-		}--[[ ,
+		}
+		{
+			type = "bool-setting",
+			name = "harvester-auto-by-default",
+			setting_type = "startup",
+			default_value = false,
+			per_user = false,
+		}
+		--[[ ,
 		{
 			type = "int-setting",
 			name = "Auto-cncharvester-Ragne",
