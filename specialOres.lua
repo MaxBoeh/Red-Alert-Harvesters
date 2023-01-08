7a836a34b2c25d849ae1711fea0f1e04f6e685af
MaxBoeh <mail@maxboehme.net>
2023-01-08 23:55:44 +0100

    - added comment to specialOres.lua

--- specialOres.lua
+++ specialOres.lua
@@ -1,3 +1,4 @@
+-- Map special ores, such as silica where the resource is called "silica" and the item is called "raw-silica".
 SpecialOres = {
     ["harvester-example-ore"] = function() return "raw-harvester-example-ore" end,
 }
