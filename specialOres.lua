b6dd34a2c8dcb6d1a37f00ec4058b782072fdf37
dvs <random.xz@gmail.com>
2024-01-02 21:24:55 +0300

    Update specialOres.lua add tiberium ore
    add tiberium ore to harvest

--- specialOres.lua
+++ specialOres.lua
@@ -1,4 +1,6 @@
 -- Map special ores, such as silica where the resource is called "silica" and the item is called "raw-silica".
 SpecialOres = {
     ["harvester-example-ore"] = function() return "raw-harvester-example-ore" end,
+    ["tibGrowthNode"] = function() return "tiberium-ore" end,
+    ["tibGrowthNode_infinite"] = function() return "tiberium-ore-blue" end,
 }
